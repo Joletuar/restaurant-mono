9bbdac698e9f2d8f4cd5b9498391f6cd7a49cc86
Johan <joletuar.dev@gmail.com>
2025-05-23 17:33:28 -0500

    fix: remove unused import and function call in index test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,5 @@
-import { helloWorld } from '@src/index';
-
 describe('index.ts', () => {
   it('should execute the index file without errors', () => {
-    helloWorld();
-
     expect(true).toBe(true);
   });
 });
